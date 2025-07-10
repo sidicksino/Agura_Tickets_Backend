816c3348c7f6717229505c226a87dc81cb08150c
nguilimy <achtahamidnguili@gmail.com>
2025-07-10 16:21:43 +0200

    Delete server directory

--- server/routes/passwordResetRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/passwordResetController');
-
-router.post('/request-reset', controller.requestReset);
-router.post('/verify-code', controller.verifyCode);
-router.post('/reset-password', controller.resetPassword);
-
-module.exports = router;
--- server/utils/generateOTP.js
+++
@@ -1,4 +0,0 @@
-module.exports = function generateOTP() {
-    return Math.floor(10000 + Math.random() * 90000).toString();
-  };
-  
